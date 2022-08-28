0b9fd72ba412352b323251612b9395b85015f70b
Nicolas Pucci <nicolaspucci1989@gmail.com>
2022-08-28 00:44:39 -0300

    initial commit

---
+++ src/main/java/dcl/museum/dclmuseummanager/DclMuseumManagerApplication.java
@@ -0,0 +1,13 @@
+package dcl.museum.dclmuseummanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DclMuseumManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DclMuseumManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dcl/museum/dclmuseummanager/DclMuseumManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dcl.museum.dclmuseummanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DclMuseumManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
